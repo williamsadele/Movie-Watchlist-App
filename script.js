3c3992f9c4a892845a865d5a5d3e948bf88ec370
Williams Adele <williams.adele.dev@gmail.com>
2026-08-15 13:54:27 +0100

    started ap

---
+++ script.js
@@ -0,0 +1,14 @@
+let movies = []
+const inptEl = document.getElementById("input-el")
+const addBtn = document.getElementById("addbtn")
+const ulEl = document.getElementById("Ul-el")
+const watchedBtn = document.queryselector(".btn-watched")
+const deleteBtn = document.querySelector(".btn-delete")
+
+function renderMovies() {
+    listItems = ""
+    for(let i=0; i < movies.length; i++)
+        if (watched){
+            strikethrough
+        }
+}
